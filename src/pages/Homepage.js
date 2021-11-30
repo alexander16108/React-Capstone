239f05c6bb57131f6fedc499f460e55b313c0a40
alexander16108 <alexandermayowa05@gmail.com>
2021-11-30 07:55:56 +0100

    setup details page and homepage

---
+++ src/pages/Homepage.js
@@ -0,0 +1,20 @@
+import { Link } from 'react-router-dom';
+import data from '../components/data';
+
+const HomePage = () => (
+  <ul className="dataUL">
+    {
+      data.map((dets) => (
+        <Link key={dets.id} to={{ pathname: `/country/${dets.country}` }}>
+          <li>
+            {dets.country}
+            <br />
+            {dets.population}
+          </li>
+        </Link>
+      ))
+    }
+  </ul>
+);
+
+export default HomePage;
